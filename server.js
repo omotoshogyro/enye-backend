62dc2b07e782f0ec79aa68a927be462ddb46f7e2
Okikiola Omotosho <omotoshookiki@gmail.com>
2021-01-25 21:40:23 +0100

    add procfile and env

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
-const PORT = 5000
+var PORT = process.env.PORT || 5000;
 
 app.get("/api/rates", (req, res) => {
   const { base, currency } = req.query;
